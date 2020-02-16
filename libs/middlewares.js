5e9e45d36f3215dce4b00cf2d8fdc3cf1e461cce
Jean <jeandobre@gmail.com>
2020-02-16 18:43:20 -0400

    Capítulo 4 a 6 do livro.

---
+++ libs/middlewares.js
@@ -0,0 +1,13 @@
+import bodyParser from "body-parser";
+
+module.exports = app => {
+	//Apenas para formatar o json e facilitar a visualização
+	//app.set("json spaces", 4);
+	app.set("port", 8080);
+	app.use(bodyParser.json());
+	app.use((req, res, next) => {
+		//middleware de pré-execução das rotas
+		delete req.body.id;
+		next();
+	});
+};
